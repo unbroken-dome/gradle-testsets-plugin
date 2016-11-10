1a28472780f4cde715b8967a62006b0e8aa56c01
Till Krullmann <till.krullmann@gmx.net>
2016-11-10 15:07:56 +0100

    Migrated DefaultTestSetContainer class from Java to Groovy

---
+++ src/main/groovy/org/unbrokendome/gradle/plugins/testsets/internal/DefaultTestSetContainer.groovy
@@ -0,0 +1,39 @@
+package org.unbrokendome.gradle.plugins.testsets.internal
+
+import org.gradle.api.internal.AbstractNamedDomainObjectContainer
+import org.gradle.internal.reflect.Instantiator
+import org.unbrokendome.gradle.plugins.testsets.dsl.ConfigurableTestSet
+import org.unbrokendome.gradle.plugins.testsets.dsl.TestSet
+import org.unbrokendome.gradle.plugins.testsets.dsl.TestSetContainer
+
+
+class DefaultTestSetContainer extends AbstractNamedDomainObjectContainer<TestSet> implements TestSetContainer {
+
+    private final TestSet predefinedUnitTestSet = new PredefinedUnitTestSet()
+
+
+    DefaultTestSetContainer(Instantiator instantiator) {
+        super(TestSet.class, instantiator)
+        super.add(predefinedUnitTestSet)
+    }
+
+
+    @Override
+    protected TestSet doCreate(String name) {
+        new DefaultTestSet(name)
+    }
+
+
+    boolean add(TestSet testSet) {
+
+        boolean added = super.add(testSet)
+
+        if (added) {
+            if (testSet instanceof ConfigurableTestSet) {
+                ((ConfigurableTestSet) testSet).extendsFrom(predefinedUnitTestSet);
+            }
+        }
+
+        return added
+    }
+}
