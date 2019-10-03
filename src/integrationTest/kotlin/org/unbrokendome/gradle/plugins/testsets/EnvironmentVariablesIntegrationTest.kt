32050f549226697a2ac87866545813bb824dacd8
forozco <forozco@palantir.com>
2019-10-03 15:20:43 -0400

    add failing test

--- src/integrationTest/kotlin/org/unbrokendome/gradle/plugins/testsets/EnvironmentVariablesIntegrationTest.kt
+++ src/integrationTest/kotlin/org/unbrokendome/gradle/plugins/testsets/EnvironmentVariablesIntegrationTest.kt
@@ -4,13 +4,31 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isNotNull
 import assertk.assertions.prop
+import org.gradle.internal.file.DefaultFileMetadata.file
 import org.gradle.testkit.runner.BuildTask
+import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.TaskOutcome
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 
 class EnvironmentVariablesIntegrationTest : AbstractGradleIntegrationTest() {
+    companion object {
+        const val TEST_FILE = """
+                    import static org.junit.jupiter.api.Assertions.*;
+                    import org.junit.jupiter.api.Test;
+                    
+                    class EnvironmentTest {
+                        
+                        @Test
+                        void shouldHaveEnvironmentAvailable() {
+                            String value = System.getenv("TESTVAR");
+                            assertEquals("TESTVALUE", value);
+                        }
+                    }
+                """
+    }
 
     @ParameterizedTest
     @ValueSource(strings = [
@@ -48,19 +66,7 @@ class EnvironmentVariablesIntegrationTest : AbstractGradleIntegrationTest() {
             """)
 
             directory("src/integrationTest/java") {
-                file("EnvironmentTest.java", """
-                    import static org.junit.jupiter.api.Assertions.*;
-                    import org.junit.jupiter.api.Test;
-                    
-                    class EnvironmentTest {
-                        
-                        @Test
-                        void shouldHaveEnvironmentAvailable() {
-                            String value = System.getenv("TESTVAR");
-                            assertEquals("TESTVALUE", value);
-                        }
-                    }
-                """)
+                file("EnvironmentTest.java", TEST_FILE)
             }
 
             val result = runGradle("integrationTest")
@@ -71,7 +77,52 @@ class EnvironmentVariablesIntegrationTest : AbstractGradleIntegrationTest() {
                 .prop("outcome", BuildTask::getOutcome)
                 .isEqualTo(TaskOutcome.SUCCESS)
         }
+    }
 
+    @Test
+    fun `should inherit existing environment variables`() {
+        directory(projectDir) {
+            file("build.gradle.kts", """
+                plugins {
+                    `java`
+                    id("org.unbroken-dome.test-sets")
+                }
+                
+                repositories {
+                    jcenter()
+                }
+                
+                testSets {
+                    createTestSet("integrationTest") { }
+                }
+                
+                dependencies {
+                    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
+                    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.5.2")
+                }
+                
+                tasks.withType<Test> {
+                    useJUnitPlatform()
+                }
+            """)
+
+            directory("src/integrationTest/java") {
+                file("EnvironmentTest.java", TEST_FILE)
+            }
+
+            val result = GradleRunner.create()
+                    .withProjectDir(projectDir)
+                    .withPluginClasspath()
+                    .withArguments("integrationTest",  "-s")
+                    .withEnvironment(mapOf("TESTVAR" to "TESTVALUE"))
+                    .build()
+
+            assertThat(result, "result")
+                    .prop("for task integrationTest") { it.task(":integrationTest") }
+                    .isNotNull()
+                    .prop("outcome", BuildTask::getOutcome)
+                    .isEqualTo(TaskOutcome.SUCCESS)
+        }
     }
 
 
