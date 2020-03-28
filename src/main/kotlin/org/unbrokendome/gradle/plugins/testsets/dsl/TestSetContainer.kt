f05e372dcc2ffc3424e7511fcef3eda252f240e7
Till Krullmann <till.krullmann@gmx.net>
2020-03-28 14:30:50 +0100

    TestSetContainer: code improvements
    - added KDoc to public methods,
    - removed an obsolete comment about the PolymorphicDomainObjectContainer constructor
    - code formatting

--- src/main/kotlin/org/unbrokendome/gradle/plugins/testsets/dsl/TestSetContainer.kt
+++ src/main/kotlin/org/unbrokendome/gradle/plugins/testsets/dsl/TestSetContainer.kt
@@ -18,83 +18,178 @@ import javax.inject.Inject
 import kotlin.reflect.KClass
 
 
+/**
+ * A container for test sets and test libraries.
+ */
 interface TestSetContainer : PolymorphicDomainObjectContainer<TestSetBase> {
 
+    /**
+     * Creates a new test set with the specified name, and adds it to the container.
+     *
+     * @param name the name of the test set
+     * @return the new [TestSet]
+     */
     @JvmDefault
     override fun create(name: String): TestSet =
-            create(name, TestSet::class.java)
+        create(name, TestSet::class.java)
 
+
+    /**
+     * Creates a new test set with the specified name, adds it to the container, and configures it
+     * with the specified action.
+     *
+     * @param name the name of the test set
+     * @param configureAction an action for configuring the test set
+     * @return the new [TestSet]
+     */
     @JvmDefault
     fun createTestSet(name: String, configureAction: Action<TestSet>): TestSet =
         create(name, TestSet::class.java, configureAction)
 
-    @JvmDefault
-    override fun create(name: String,
-                        @DelegatesTo(TestSet::class, strategy = Closure.DELEGATE_FIRST)
-                        configureClosure: Closure<Any>): TestSet =
-            create(name, TestSet::class.java, configureClosure.toAction())
 
+    /**
+     * Creates a new test set with the specified name, adds it to the container, and configures it
+     * with the specified closure.
+     *
+     * This variant is intended for Groovy DSL support, with an annotated closure parameter for better
+     * IDE support.
+     *
+     * @param name the name of the test set
+     * @param configureClosure a closure for configuring the test set
+     * @return the new [TestSet]
+     */
+    @JvmDefault
+    override fun create(
+        name: String,
+        @DelegatesTo(TestSet::class, strategy = Closure.DELEGATE_FIRST)
+        configureClosure: Closure<Any>
+    ): TestSet =
+        create(name, TestSet::class.java, configureClosure.toAction())
+
+
+    /**
+     * Creates a new test library with the specified name, and adds it to the container.
+     *
+     * @param name the name of the test library
+     * @return the new [TestLibrary]
+     */
     @JvmDefault
     fun createLibrary(name: String): TestLibrary =
-            create(name, TestLibrary::class.java)
+        create(name, TestLibrary::class.java)
 
+
+    /**
+     * Creates a new test library with the specified name, adds it to the container, and configures it with
+     * the specified action.
+     *
+     * @param name the name of the test library
+     * @param configureAction an action for configuring the test library
+     * @return the new [TestLibrary]
+     */
     @JvmDefault
     fun createLibrary(name: String, configureAction: Action<TestLibrary>): TestLibrary =
-            create(name, TestLibrary::class.java, configureAction)
+        create(name, TestLibrary::class.java, configureAction)
 
+
+    /**
+     * Creates a new test library with the specified name, adds it to the container, and configures it with
+     * the specified closure.
+     *
+     * This variant is intended for Groovy DSL support, with an annotated closure parameter for better
+     * IDE support.
+     *
+     * @param name the name of the test library
+     * @param configureClosure an closure for configuring the test library
+     * @return the new [TestLibrary]
+     */
     @JvmDefault
-    fun createLibrary(name: String,
-                      @DelegatesTo(TestLibrary::class, strategy = Closure.DELEGATE_FIRST)
-                      configureClosure: Closure<*>): TestLibrary =
-            createLibrary(name, configureClosure.toAction())
+    fun createLibrary(
+        name: String,
+        @DelegatesTo(TestLibrary::class, strategy = Closure.DELEGATE_FIRST)
+        configureClosure: Closure<*>
+    ): TestLibrary =
+        createLibrary(name, configureClosure.toAction())
 
 
+    /**
+     * A [NamedDomainObjectContainer] that wraps this container but presents only the test libraries.
+     */
     @JvmDefault
     val libraries: NamedDomainObjectContainer<TestLibrary>
         get() = containerWithType(TestLibrary::class.java)
 
 
+    /**
+     * Configures the test libraries with the specified action.
+     *
+     * @param action an action that configures the [NamedDomainObjectContainer] presenting only the test libraries
+     */
     @JvmDefault
     fun libraries(action: Action<NamedDomainObjectContainer<TestLibrary>>) =
-            action.execute(libraries)
+        action.execute(libraries)
 
 
+    /**
+     * Configures the test set with the specified name, creating it if it does not exist.
+     *
+     * Intended for Kotlin DSL support (in Groovy DSL, containers will automatically support domain object names
+     * as member functions)
+     *
+     * @receiver the name of the test set
+     * @param configureAction an action for configuring the [TestSet]
+     * @return the [TestSet]
+     */
     operator fun String.invoke(configureAction: TestSet.() -> Unit = {}): TestSet =
-            maybeCreate(this, TestSet::class.java).also(configureAction)
+        maybeCreate(this, TestSet::class.java).also(configureAction)
 
 
+    /**
+     * Configures the test set or test library with the specified name, creating it if it does not exist.
+     *
+     * Intended for Kotlin DSL support (in Groovy DSL, containers will automatically support domain object names
+     * as member functions)
+     *
+     * @param type the type of object as a Kotlin [KClass] (either `TestSet::class` or `TestLibrary::class`)
+     * @return the [TestSet] or [TestLibrary]
+     * @throws ClassCastException if the item already exists in the container, but with a different type
+     */
     operator fun <T : TestSetBase> String.invoke(type: KClass<T>, configureAction: T.() -> Unit = {}): T =
-            maybeCreate(this, type.java).also(configureAction)
+        maybeCreate(this, type.java).also(configureAction)
 }
 
 
 private open class DefaultTestSetContainer
-@Inject constructor(private val project: Project, instantiator: Instantiator)
-// Use this constructor, as they are still supporting it because 'nebula.lint' uses it
-    : DefaultPolymorphicDomainObjectContainer<TestSetBase>(
-        TestSetBase::class.java,
-        instantiator,
-        CollectionCallbackActionDecorator.NOOP),
-        TestSetContainer {
+@Inject constructor(
+    private val project: Project,
+    instantiator: Instantiator
+) : DefaultPolymorphicDomainObjectContainer<TestSetBase>(
+    TestSetBase::class.java,
+    instantiator,
+    CollectionCallbackActionDecorator.NOOP
+),
+    TestSetContainer {
 
     private companion object {
         val CREATEABLE_TYPES: Set<Class<out TestSetBase>> = setOf(
-                TestSet::class.java, TestLibrary::class.java)
+            TestSet::class.java, TestLibrary::class.java
+        )
     }
 
 
     @Suppress("LeakingThis")
-    private val unitTestSet = PredefinedUnitTestSet(this,
-            project.sourceSets[SourceSet.TEST_SOURCE_SET_NAME])
-            .also { add(it) }
+    private val unitTestSet = PredefinedUnitTestSet(
+        this,
+        project.sourceSets[SourceSet.TEST_SOURCE_SET_NAME]
+    )
+        .also { add(it) }
 
 
     override fun create(name: String): TestSet =
-            create(name, TestSet::class.java)
+        create(name, TestSet::class.java)
 
 
     override fun create(name: String, configureClosure: Closure<Any>): TestSet =
-            create(name, TestSet::class.java, configureClosure.toAction())
+        create(name, TestSet::class.java, configureClosure.toAction())
 
 
     private val entityInstantiator = object : NamedEntityInstantiator<TestSetBase> {
@@ -104,7 +199,7 @@ private open class DefaultTestSetContainer
             return when (type) {
                 TestSet::class.java ->
                     project.objects.newTestSet(this@DefaultTestSetContainer, name, sourceSet)
-                            .apply { extendsFrom(unitTestSet) }
+                        .apply { extendsFrom(unitTestSet) }
                 TestLibrary::class.java ->
                     project.objects.newTestLibrary(this@DefaultTestSetContainer, name, sourceSet)
                 else ->
@@ -125,34 +220,36 @@ private open class DefaultTestSetContainer
      * @param name the name of the new test set
      */
     private fun createSourceSetForTestSet(name: String): SourceSet =
-            project.sourceSets.create(NamingConventions.sourceSetName(name))
-                    .also { sourceSet ->
-                        sourceSet.compileClasspath = project.files(
-                                project.sourceSets["main"].output,
-                                project.configurations[sourceSet.compileClasspathConfigurationName])
-                        sourceSet.runtimeClasspath = project.files(
-                                sourceSet.output,
-                                project.sourceSets["main"].output,
-                                project.configurations[sourceSet.runtimeClasspathConfigurationName])
-                    }
+        project.sourceSets.create(NamingConventions.sourceSetName(name))
+            .also { sourceSet ->
+                sourceSet.compileClasspath = project.files(
+                    project.sourceSets["main"].output,
+                    project.configurations[sourceSet.compileClasspathConfigurationName]
+                )
+                sourceSet.runtimeClasspath = project.files(
+                    sourceSet.output,
+                    project.sourceSets["main"].output,
+                    project.configurations[sourceSet.runtimeClasspathConfigurationName]
+                )
+            }
 
 
     override fun doCreate(name: String): TestSet =
-            doCreate(name, TestSet::class.java)
+        doCreate(name, TestSet::class.java)
 
 
     override fun <U : TestSetBase> doCreate(name: String, type: Class<U>): U =
-            entityInstantiator.create(name, type)
+        entityInstantiator.create(name, type)
 
 
     override fun getCreateableTypes(): Set<Class<out TestSetBase>> =
-            CREATEABLE_TYPES
+        CREATEABLE_TYPES
 
 
     override fun getEntityInstantiator(): NamedEntityInstantiator<TestSetBase> =
-            entityInstantiator
+        entityInstantiator
 }
 
 
 internal fun Project.testSetContainer(instantiator: Instantiator): TestSetContainer =
-        objects.newInstance(DefaultTestSetContainer::class.java, this, instantiator)
+    objects.newInstance(DefaultTestSetContainer::class.java, this, instantiator)
